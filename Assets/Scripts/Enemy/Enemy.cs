5ba9c9febe56bf45963f45710f9394331b8ef6cd
Daniil <horpuru@gmail.com>
2023-10-07 13:54:21 +0300

    Create Enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,7 @@
+﻿namespace Enemy
+{
+    public abstract class Enemy
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Interfaces/IAttacking.cs
@@ -0,0 +1,7 @@
+﻿namespace Interfaces
+{
+    public interface IAttacking
+    {
+        void Attack(Enemy.Enemy entity);
+    }
+}

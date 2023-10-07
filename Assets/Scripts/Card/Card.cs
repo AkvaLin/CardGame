d64c95648d38be56475e691026646d17a6379593
Daniil <horpuru@gmail.com>
2023-10-07 14:22:50 +0300

    Change version

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -2,11 +2,16 @@
 
 namespace Card
 {
-    public class Card : Entity, IAttacking
+    public class Card : Entity, IAttacking, IDamageable
     {
         public void Attack(Enemy.Enemy enemy)
         {
             enemy.GetDamage(damage);
+        }
+
+        public void GetDamage(int damage)
+        {
+            hp -= damage;
         }
     }
 }
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Interfaces;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Player
+{
+    public class Player : MonoBehaviour, IPush
+    {
+        [SerializeField] private List<Card.Card> cardsOnHand = new List<Card.Card>();
+
+        public List<Card.Card> GetCardOnHand => cardsOnHand;
+
+        public Card.Card PushCard(int index) => cardsOnHand[index];
+    }
+}
